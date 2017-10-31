817c930ce5c42fb5dc954b1e1f801df2493f6069
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2017-10-31 10:29:25 +0800

    use alt shift v for normal keyboard settings since there is an issue that alt is always there wherever key entered

--- normal_keyboard_settings.sh
+++ normal_keyboard_settings.sh
@@ -2,9 +2,8 @@
 
 osascript /Users/josephyao/Documents/personal_workspace/shortcut_key_update_workflow/paste_before_click_record_field.scpt
 
-/usr/local/bin/cliclick c:1011,294 kd:shift,cmd t:v ku:shift,cmd
+/usr/local/bin/cliclick c:1011,294 kd:alt,shift t:v ku:alt,shift
 
 osascript /Users/josephyao/Documents/personal_workspace/shortcut_key_update_workflow/paste_after_click_record_field.scpt
 
 osascript /Users/josephyao/Documents/personal_workspace/shortcut_key_update_workflow/normal_input_source.scpt
-
