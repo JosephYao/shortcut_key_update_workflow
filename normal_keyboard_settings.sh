5cd0f01fa32059174df538a9a6faa993de01baf7
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2017-11-01 19:11:58 +0800

    switch back to programmer dvorak layout for normal

--- normal_keyboard_settings.sh
+++ normal_keyboard_settings.sh
@@ -7,3 +7,5 @@ osascript /Users/josephyao/Documents/personal_workspace/shortcut_key_update_work
 osascript /Users/josephyao/Documents/personal_workspace/shortcut_key_update_workflow/paste_after_click_record_field.scpt
 
 osascript /Users/josephyao/Documents/personal_workspace/shortcut_key_update_workflow/normal_input_source.scpt
+
+osascript /Users/josephyao/Documents/personal_workspace/shortcut_key_update_workflow/normal_switch_layout.scpt
---
+++ normal_switch_layout.scpt
@@ -0,0 +1,3 @@
+tell application "System Events" to tell process "SystemUIServer"
+  tell (1st menu bar item of menu bar 1 whose description is "text input") to {click, click (menu 1's menu item "Programmer Dvorak")}
+end tell
