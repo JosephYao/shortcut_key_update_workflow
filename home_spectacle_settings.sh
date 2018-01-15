be4aeebdba43e789cc02874565cae1a717b4bf42
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2018-01-15 20:55:23 +0800

    create workflow for spectacle full screen shortcut

---
+++ home_spectacle_settings.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+osascript /Users/josephyao/Documents/personal_workspace/shortcut_key_update_workflow/spectacle_before_click_record_field.scpt
+
+/usr/local/bin/cliclick c:760,357 kd:ctrl,cmd t:f ku:ctrl,cmd
+
+osascript /Users/josephyao/Documents/personal_workspace/shortcut_key_update_workflow/spectacle_after_click_record_field.scpt
---
+++ spectacle_after_click_record_field.scpt
@@ -0,0 +1,2 @@
+delay 1
+tell application "System Events" to click UI Element 30 of window "Spectacle 1.2" of application process "Spectacle"
