df2f76091b12d12dddb81c391ee37d148d80a347
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2018-01-17 10:34:39 +0800

    office: set shortcut of full screen

---
+++ office_spectacle_settings.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+osascript /Users/josephyao/Documents/personal_workspace/shortcut_key_update_workflow/spectacle_before_click_record_field.scpt
+
+/usr/local/bin/cliclick c:763,388 kd:alt,shift t:f ku:alt,shift
+
+osascript /Users/josephyao/Documents/personal_workspace/shortcut_key_update_workflow/spectacle_after_click_record_field.scpt
