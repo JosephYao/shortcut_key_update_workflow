db1df0b7f9b214060385d667dc96c2f1e25c7a89
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2018-01-20 17:10:33 +0800

    wechat: set screen capture shortcut for wechat

---
+++ office_wechat_settings.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+/usr/local/bin/cliclick kd:cmd t:,
+
+osascript /Users/josephyao/Documents/personal_workspace/shortcut_key_update_workflow/wechat_before_click_record_field.scpt
+
+/usr/local/bin/cliclick c:903,467 kd:ctrl,cmd t:c ku:ctrl,cmd
+
+osascript /Users/josephyao/Documents/personal_workspace/shortcut_key_update_workflow/wechat_after_click_record_field.scpt
---
+++ wechat_before_click_record_field.scpt
@@ -0,0 +1,3 @@
+tell application "System Events" to click UI Element "Shortcuts" of tool bar 1 of window "General" of application process "WeChat"
+tell application "System Events" to click UI Element 5 of window "Shortcuts" of application process "WeChat"
+delay 1
