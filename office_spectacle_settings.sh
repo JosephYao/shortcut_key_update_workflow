a3847a803fb4b539a853accdcb301faf5edb08ed
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2018-02-07 10:40:36 +0800

    fix the click position of office spectacle shortcut

--- office_spectacle_settings.sh
+++ office_spectacle_settings.sh
@@ -2,6 +2,6 @@
 
 osascript /Users/josephyao/Documents/personal_workspace/shortcut_key_update_workflow/spectacle_before_click_record_field.scpt
 
-/usr/local/bin/cliclick c:763,388 kd:alt,shift t:f ku:alt,shift
+/usr/local/bin/cliclick c:883,373 kd:alt,shift t:f ku:alt,shift
 
 osascript /Users/josephyao/Documents/personal_workspace/shortcut_key_update_workflow/spectacle_after_click_record_field.scpt
