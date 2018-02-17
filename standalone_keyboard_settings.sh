f5f05ca5830ce972d3b9605f24e9f9bd8b60305b
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2018-02-17 13:19:18 +0800

    create standalone mode, it means use no external keyboard or monitor

---
+++ standalone_keyboard_settings.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+osascript /Users/josephyao/Documents/personal_workspace/shortcut_key_update_workflow/paste_before_click_record_field.scpt
+
+/usr/local/bin/cliclick c:900,290 kd:shift,alt t:v ku:shift,alt
+
+osascript /Users/josephyao/Documents/personal_workspace/shortcut_key_update_workflow/paste_after_click_record_field.scpt
+
+osascript /Users/josephyao/Documents/personal_workspace/shortcut_key_update_workflow/normal_input_source.scpt
+
+osascript /Users/josephyao/Documents/personal_workspace/shortcut_key_update_workflow/normal_switch_layout.scpt
