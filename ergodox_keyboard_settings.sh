498848e2da5e886a9411ffeb0b249c88d4d23c91
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2018-03-17 16:53:11 +0800

    make the home and office shortcut key for wechat and paste consistent

--- ergodox_keyboard_settings.sh
+++ ergodox_keyboard_settings.sh
@@ -2,7 +2,7 @@
 
 osascript /Users/josephyao/Documents/personal_workspace/shortcut_key_update_workflow/paste_before_click_record_field.scpt
 
-/usr/local/bin/cliclick c:888,288 kd:shift,cmd t:v ku:shift,cmd
+/usr/local/bin/cliclick c:888,288 kd:shift,alt t:v ku:shift,alt
 
 osascript /Users/josephyao/Documents/personal_workspace/shortcut_key_update_workflow/paste_after_click_record_field.scpt
 
--- home_wechat_settings.sh
+++ home_wechat_settings.sh
@@ -2,6 +2,6 @@
 
 osascript /Users/josephyao/Documents/personal_workspace/shortcut_key_update_workflow/wechat_before_click_record_field.scpt
 
-/usr/local/bin/cliclick c:903,467 kd:cmd,alt t:c ku:cmd,alt
+/usr/local/bin/cliclick c:903,467 kd:cmd,ctrl t:c ku:cmd,ctrl
 
 osascript /Users/josephyao/Documents/personal_workspace/shortcut_key_update_workflow/wechat_after_click_record_field.scpt
