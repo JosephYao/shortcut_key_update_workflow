6d45d0b057cde3cb9ee7c52813e44fbf884e19d2
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2020-09-09 18:43:42 +0800

    :sparkles: make standalone setting paste shortcut really working

--- standalone_keyboard_settings.sh
+++ standalone_keyboard_settings.sh
@@ -2,10 +2,10 @@
 
 osascript /Users/josephyao/Documents/personal_workspace/shortcut_key_update_workflow/paste_before_click_record_field.scpt
 
-/usr/local/bin/cliclick c:900,290 kd:cmd,ctrl t:v ku:cmd,ctrl
+/usr/local/bin/cliclick c:888,250 c:968,250 c:888,250 kd:cmd,ctrl t:v ku:cmd,ctrl
 
 osascript /Users/josephyao/Documents/personal_workspace/shortcut_key_update_workflow/paste_after_click_record_field.scpt
 
-osascript /Users/josephyao/Documents/personal_workspace/shortcut_key_update_workflow/normal_input_source.scpt
-
-osascript /Users/josephyao/Documents/personal_workspace/shortcut_key_update_workflow/normal_switch_layout.scpt
+#osascript /Users/josephyao/Documents/personal_workspace/shortcut_key_update_workflow/normal_input_source.scpt
+#
+#osascript /Users/josephyao/Documents/personal_workspace/shortcut_key_update_workflow/normal_switch_layout.scpt
