7bf650131e612326c45b9d18a0d7e2786a26b95f
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2020-09-09 18:39:30 +0800

    :sparkles: make home setting paste shortcut really working

--- ergodox_keyboard_settings.sh
+++ ergodox_keyboard_settings.sh
@@ -2,7 +2,7 @@
 
 osascript /Users/josephyao/Documents/personal_workspace/shortcut_key_update_workflow/paste_before_click_record_field.scpt
 
-/usr/local/bin/cliclick c:888,250 kd:cmd,ctrl t:v ku:cmd,ctrl
+/usr/local/bin/cliclick c:888,250 c:968,250 c:888,250 kd:cmd,ctrl t:v ku:cmd,ctrl
 
 osascript /Users/josephyao/Documents/personal_workspace/shortcut_key_update_workflow/paste_after_click_record_field.scpt
 
--- paste_after_click_record_field.scpt
+++ paste_after_click_record_field.scpt
@@ -1,3 +1,3 @@
 delay 1
 tell application "System Events" to click UI Element "General" of tool bar 1 of window "Shortcuts" of application process "Paste"
-tell application "System Events" to click UI Element 2 of window "General" of application process "Paste"
+tell application "System Events" to click UI Element 3 of window "General" of application process "Paste"
