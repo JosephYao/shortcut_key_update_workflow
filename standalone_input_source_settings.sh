6c6dc205589c91aaf0db5f17a2fc84f004f15fd0
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2020-09-13 12:40:29 +0800

    :bug: uncomment the script

--- standalone_input_source_settings.sh
+++ standalone_input_source_settings.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-#osascript /Users/josephyao/Documents/personal_workspace/shortcut_key_update_workflow/normal_input_source.scpt
+osascript /Users/josephyao/Documents/personal_workspace/shortcut_key_update_workflow/normal_input_source.scpt
 
 osascript /Users/josephyao/Documents/personal_workspace/shortcut_key_update_workflow/normal_switch_layout.scpt
