9afa9730937957ec3599717378d68e4130416502
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2020-09-13 12:50:38 +0800

    :sparkles: update the standalone spectacle shortcut with right key

--- standalone_spectacle_settings.sh
+++ standalone_spectacle_settings.sh
@@ -2,6 +2,6 @@
 
 osascript /Users/josephyao/Documents/personal_workspace/shortcut_key_update_workflow/spectacle_before_click_record_field.scpt
 
-/usr/local/bin/cliclick c:760,360 kd:cmd,ctrl t:f ku:cmd,ctrl
+/usr/local/bin/cliclick c:760,360 kd:cmd,ctrl t:y ku:cmd,ctrl
 
 osascript /Users/josephyao/Documents/personal_workspace/shortcut_key_update_workflow/spectacle_after_click_record_field.scpt
